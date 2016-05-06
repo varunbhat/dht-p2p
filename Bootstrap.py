c4fddcc20b66cd52c0bfdcf942b27ae0db8f20e4
Varun Bhat <varunb@colostate.edu>
2016-05-06 17:53:38 -0600

    fixed bugs

--- Bootstrap.py
+++ Bootstrap.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 import asyncio
 import configparser
@@ -9,6 +10,7 @@ import re
 from aiocoap import resource
 from aiocoap.message import Message
 from aiocoap.protocol import Context
+from chord import Chord
 
 
 def arguement_parser():
@@ -37,6 +39,7 @@ class BootstrapServer(resource.Resource):
     def __init__(self):
         super(BootstrapServer, self).__init__()
         self.area_router_map = {}
+        self.chord = Chord()
 
     @asyncio.coroutine
     def render_put(self, request):
@@ -69,6 +72,7 @@ class BootstrapServer(resource.Resource):
 
         if '/boostrap/areaindex' in routers:
             self.area_router_map[key] = addr
+            print(self.area_router_map)
         else:
             # Todo: register with the next available area (Use google maps / gridfs)
             logging.warning('No area routers found. Dropping registration Request')
@@ -87,29 +91,42 @@ class BootstrapServer(resource.Resource):
         resp = yield from context.request(request).response
         print('Result: %s\n%r' % (resp.code, resp.payload))
 
+    def render_get(self, request):
+        payload = json.loads(request.payload.decode('utf8'))
+        key = self.chord.generate_key(payload['area'])
+        print('Key',key)
+        res = self.area_router_map.get(str(key))
+        print(self.area_router_map)
+        print('res',res)
+        print(payload)
 
-class BootstrapDomainUpdate:
+        if res is not None:
+            context = yield from Context.create_client_context()
+            request = Message(code=aiocoap.GET, payload=json.dumps({'sensor': payload['sensor']}).encode('utf8'))
+            request.opt.uri_host = res[0]
+            request.opt.uri_port = res[1]
+            request.opt.uri_path = ('node', 'register')
+            resp = yield from context.request(request).response
+            print('resp_msg', resp.payload.decode('utf8'))
+            data = json.loads(resp.payload.decode('utf8'))
+            print(data)
 
-    def __init__(self):
-        pass
+        return aiocoap.Message(code=aiocoap.CONTENT, payload=json.dumps(data).encode('utf8'))
 
-    @asyncio.coroutine
-    def updateIpAddress(self):
-        "http://username:password@dynupdate.no-ip.com/nic/update?hostname=mytest.testdomain.com&myip=1.2.3.4
 
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
 
 def main():
+
     logging.debug('Starting the Bootstrap Server')
     root = resource.Site()
-    root.add_resource(('bootstrap','register'), BootstrapServer())
+    root.add_resource(('query',), BootstrapServer())
     logging.debug('starting at Port:%s' % CONFIG.bs_address[1])
     asyncio.async(aiocoap.Context.create_server_context(root, bind=('', CONFIG.bs_address[1])))
     asyncio.get_event_loop().run_forever()
 
 
 if __name__ == '__main__':
-    print(1)
     main()
